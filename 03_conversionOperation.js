5e275c740fac032561260aae6fc0f411ba6e1ecf
Md Raza <129743200+mdraza77@users.noreply.github.com>
2025-03-11 18:14:01 +0000

    Conversion of data typess

---
+++ 03_conversionOperation.js
@@ -0,0 +1,18 @@
+// let score = 29;
+let score = "29";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valInNumber = Number(score);
+console.log(valInNumber);
+console.log(typeof(valInNumber));
+
+let bolean = "";
+let inBool = Boolean(bolean);
+console.log(inBool);
+console.log(typeof(inBool));
+
+let somenum = 22;
+let inStr = String(somenum);
+console.log(inStr);
+console.log(typeof(inStr));
