c3a2b5d9a803f6e0520225b65b0ee8b550cbbf5b
Md Raza <129743200+mdraza77@users.noreply.github.com>
2025-03-11 17:57:30 +0000

    variables

---
+++ 01_variables.js
@@ -0,0 +1,5 @@
+const name = "Md Raza";
+let age = 18;
+age = 19
+console.table([name, age]);
+// console.log(name);
---
+++ 02_dataTypes.js
@@ -0,0 +1,8 @@
+"use strict"; // treat all JS code as newer version
+let name = "Md Raza";
+let age = 19;
+console.table([name, age]);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
