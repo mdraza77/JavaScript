5ad411ce3bdb9c99ca3bf90f9a5cec34455bbe30
Md Raza <129743200+mdraza77@users.noreply.github.com>
2025-03-12 13:55:21 +0000

    numbers

---
+++ 06_nums_and_math.js
@@ -0,0 +1,19 @@
+const number = 100;
+const balance = new Number(100);
+// console.log(balance);
+// console.log(typeof balance);
+// console.log(balance.toFixed(2));
+// console.log(typeof balance);
+
+const othernum = 125.258852;
+// console.log(othernum.toPrecision(5));
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++ Maths ++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-50));
+console.log(Math.round(5.6));
+console.log(Math.ceil(5.2));
