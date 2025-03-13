737abc1f524af6a39d675df0fa115cc67b2956e8
Md Raza <129743200+mdraza77@users.noreply.github.com>
2025-03-13 15:09:51 +0000

    Nums and Maths

--- 06_nums_and_math.js
+++ 06_nums_and_math.js
@@ -9,11 +9,30 @@ const othernum = 125.258852;
 // console.log(othernum.toPrecision(5));
 
 const hundreds = 1000000;
-console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN'));
 
 // ++++++++++++++++++ Maths ++++++++++++++++++
 
 // console.log(Math);
 // console.log(Math.abs(-50));
-console.log(Math.round(5.6));
-console.log(Math.ceil(5.2));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.2));
+// console.log(Math.min(1,2,3,4,5,6));
+// console.log(Math.max(1,2,3,4,5,6));
+
+// console.log(Math.round((Math.random()*10)+1));
+
+const min = 10;
+const max = 20;
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+// for my learning purpose
+// let arr = [];
+// for(let i = 0; i<20; i++) {
+//     const counter = Math.floor(Math.random() * (max - min + 1)) + min;
+//     arr[i] = counter;
+// }
+// console.log(arr.sort());
+
